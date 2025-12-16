87af439760609aebe493c8c2c77e744417201957
Pavanteja-21 <pavan.singaraju21@gmail.com>
2025-12-16 15:09:55 +0530

    Completed Register, Login and Change password in AuthController

---
+++ backend/ecommerce/src/main/java/com/pavan/ecommerce/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.pavan.ecommerce.controller;
+
+import com.pavan.ecommerce.dto.ChangePasswordRequest;
+import com.pavan.ecommerce.dto.LoginRequest;
+import com.pavan.ecommerce.model.User;
+import com.pavan.ecommerce.service.JwtService;
+import com.pavan.ecommerce.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        final UserDetails userDetails = userService.getUserByEmail(loginRequest.getEmail());
+        final String jwt = jwtService.generateToken(userDetails);
+        return ResponseEntity.ok(jwt);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@Valid @RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        userService.changePassword(email, request);
+        return ResponseEntity.ok().body("Password Changed");
+    }
+
+}
